90857ae23a2944c00821cee9dec2c64e77ae7cdb
Tim Wenger <timwenger94@gmail.com>
2021-04-04 15:34:59 -0700

    parse value refactoring

--- InventoryTest/ParseValueTests.cs
+++ InventoryTest/ParseValueTests.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 namespace InventoryTest
 {
-    public class FileParsingTests
+    public class ParseValueTests
     {
         [SetUp]
         public void Init()
@@ -50,90 +50,99 @@ namespace InventoryTest
         public void HasNoDescription_ExpectError()
         {
             FolderSummary folderSummary = new FolderSummary("folder name");
-
             RecursiveCounter.ParseFileToRecord("$10", folderSummary);
-            //Assert.AreEqual(0f, folderSummary.Files.TotalWorth, 0.01f);
-            Assert.AreEqual(1, folderSummary.Files.GetCollectionErrorsCopy().Count);
+
+            List<CsvRecord> errors = folderSummary.Files.GetCollectionErrorsCopy();
+            Assert.AreEqual(1, errors.Count);
+            Assert.AreEqual(new Error("$10", Error.Type.value).Print(), errors[0].Description);
         }
 
         [Test]
         public void DoesntStartwithDollarSign_ExpectError()
         {
             FolderSummary folderSummary = new FolderSummary("folder name");
-
             RecursiveCounter.ParseFileToRecord("10 apple", folderSummary);
-            //Assert.AreEqual(0f, folderSummary.Files.TotalWorth, 0.01f);
-            Assert.AreEqual(1, folderSummary.Files.GetCollectionErrorsCopy().Count);
+
+            List<CsvRecord> errors = folderSummary.Files.GetCollectionErrorsCopy();
+            Assert.AreEqual(1, errors.Count);
+            Assert.AreEqual(new Error("10 apple", Error.Type.value).Print(), errors[0].Description);
         }
 
         [Test]
         public void ValueHasCommaInIt_ExpectError()
         {
             FolderSummary folderSummary = new FolderSummary("folder name");
-
             RecursiveCounter.ParseFileToRecord("$10,000 apple", folderSummary);
-            //Assert.AreEqual(0f, folderSummary.Files.TotalWorth, 0.01f);
-            Assert.AreEqual(1, folderSummary.Files.GetCollectionErrorsCopy().Count);
+
+            List<CsvRecord> errors = folderSummary.Files.GetCollectionErrorsCopy();
+            Assert.AreEqual(1, errors.Count);
+            Assert.AreEqual(new Error("$10,000 apple", Error.Type.value).Print(), errors[0].Description);
         }
 
         [Test]
         public void Has1Decimal_ExpectError()
         {
             FolderSummary folderSummary = new FolderSummary("folder name");
-
             RecursiveCounter.ParseFileToRecord("$10.1 apple", folderSummary);
-            //Assert.AreEqual(0f, folderSummary.Files.TotalWorth, 0.01f);
-            Assert.AreEqual(1, folderSummary.Files.GetCollectionErrorsCopy().Count);
+
+            List<CsvRecord> errors = folderSummary.Files.GetCollectionErrorsCopy();
+            Assert.AreEqual(1, errors.Count);
+            Assert.AreEqual(new Error("$10.1 apple", Error.Type.value).Print(), errors[0].Description);
         }
 
         [Test]
         public void TooManyDecimals_ExpectError()
         {
             FolderSummary folderSummary = new FolderSummary("folder name");
-
             RecursiveCounter.ParseFileToRecord("$10.108 apple", folderSummary);
-            //Assert.AreEqual(0f, folderSummary.Files.TotalWorth, 0.01f);
-            Assert.AreEqual(1, folderSummary.Files.GetCollectionErrorsCopy().Count);
+
+            List<CsvRecord> errors = folderSummary.Files.GetCollectionErrorsCopy();
+            Assert.AreEqual(1, errors.Count);
+            Assert.AreEqual(new Error("$10.108 apple", Error.Type.value).Print(), errors[0].Description);
         }
 
         [Test]
         public void StartsWithDecimal_ExpectError()
         {
             FolderSummary folderSummary = new FolderSummary("folder name");
-
             RecursiveCounter.ParseFileToRecord("$.10 apple", folderSummary);
-            //Assert.AreEqual(0f, folderSummary.Files.TotalWorth, 0.01f);
-            Assert.AreEqual(1, folderSummary.Files.GetCollectionErrorsCopy().Count);
+
+            List<CsvRecord> errors = folderSummary.Files.GetCollectionErrorsCopy();            
+            Assert.AreEqual(1, errors.Count);
+            Assert.AreEqual(new Error("$.10 apple", Error.Type.value).Print(), errors[0].Description);
         }
 
         [Test]
         public void BadSpacing_ExpectError()
         {
             FolderSummary folderSummary = new FolderSummary("folder name");
-
             RecursiveCounter.ParseFileToRecord("$ 10 apple", folderSummary);
-            //Assert.AreEqual(0f, folderSummary.Files.TotalWorth, 0.01f);
-            Assert.AreEqual(1, folderSummary.Files.GetCollectionErrorsCopy().Count);
+
+            List<CsvRecord> errors = folderSummary.Files.GetCollectionErrorsCopy();
+            Assert.AreEqual(1, errors.Count);
+            Assert.AreEqual(new Error("$ 10 apple", Error.Type.value).Print(), errors[0].Description);
         }
 
         [Test]
         public void NoSpacing_ExpectError()
         {
             FolderSummary folderSummary = new FolderSummary("folder name");
-
             RecursiveCounter.ParseFileToRecord("$10apple", folderSummary);
-            //Assert.AreEqual(0f, folderSummary.Files.TotalWorth, 0.01f);
-            Assert.AreEqual(1, folderSummary.Files.GetCollectionErrorsCopy().Count);
+
+            List<CsvRecord> errors = folderSummary.Files.GetCollectionErrorsCopy();
+            Assert.AreEqual(1, errors.Count);
+            Assert.AreEqual(new Error("$10apple", Error.Type.value).Print(), errors[0].Description);
         }
 
         [Test]
-        public void WrongOrder_ExpectError()
+        public void HasUnexpectedDate_ExpectError()
         {
             FolderSummary folderSummary = new FolderSummary("folder name");
-
             RecursiveCounter.ParseFileToRecord("2020,12,30 apple $10", folderSummary);
-            //Assert.AreEqual(0f, folderSummary.Files.TotalWorth, 0.01f);
-            Assert.AreEqual(1, folderSummary.Files.GetCollectionErrorsCopy().Count);
+
+            List<CsvRecord> errors = folderSummary.Files.GetCollectionErrorsCopy();
+            Assert.AreEqual(1, errors.Count);
+            Assert.AreEqual(new Error("2020,12,30 apple $10", Error.Type.value).Print(), errors[0].Description);
         }
 
         
